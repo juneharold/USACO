c6e2aa76fc265ff9d05be1e5754e4a67857f1838
Charrybot <junehahwang@gmail.com>
2021-01-05 02:57:18 +0900

    usaco practice

--- usaco_practice/bronze_A/A4_array_two/Blocked_Billboard.cpp
+++ usaco_practice/bronze_A/A4_array_two/Blocked_Billboard.cpp
@@ -12,80 +12,42 @@ int main()
     cin >> x3 >> y3 >> x4 >> y4;
     cin >> x >> y >> X >> Y;
     // check if x1 < x3
-    if (x1>x3)
+    int plane[2001][2001]={}; // ranges from -1000 to 1000
+    // first billboard
+    for (int i=y1; i<y2; i++)
     {
-        int t1=x1, t2=y1, t3=x2, t4=y2;
-        x1=x3;
-        y1=y3;
-        x2=x4;
-        y2=y4;
-        x3=t1;
-        y3=t2;
-        x4=t3;
-        y4=t4;
-    }
-    // sum area of billboards
-    int total_area = (x2-x1)*(y2-y1) + (x4-x3)*(y4-y3);
-    int final_area = 0;
-    // locating the position of lower left of truck
-    if (x<x1)
-    {
-        if (X<x1) // doesn't cover
+        for (int j=x1; j<x2; j++)
         {
-            final_area = total_area - 0;
-        }
-        else if (X>=x1 and X<x2) // covers part of first billboard
-        {
-            if (Y>y1 and Y<=y2) // covers part of first billboard
-            {
-                final_area = total_area - (X-x1)*(Y-y1);
-            }
-            else if (Y>y1 and Y>y2)
-            {
-                final_area = total_area - (X-x1)*(y2-y1);
-            }
-        }
-        else if (X>=x2 and X<x3) // covers part of first billboard
-        {
-            if (Y>y1 and Y<=y2) // covers part of first billboard
-            {
-                final_area = total_area - (x2-x1)*(Y-y1);
-            }
-            else if (Y>y1 and Y>y2)
-            {
-                final_area = total_area - (x2-x1)*(y2-y1);
-            }
-        }
-        else if (X>=x3 and X<x4) // covers first and second 
-        {
-            if (Y>y1 and Y<=y2) 
-            {
-                
-            }
-            else if (Y>y1 and Y>y2)
-            {
-                
-            }
-        }
-        else if (X>=x4)
-        {
-
+            plane[j+1000][i+1000]=1;
         }
     }
-    else if (x>=x1 and x<x2)
+    // second billboard
+    for (int k=y3; k<y4; k++)
     {
-
+        for (int l=x3; l<x4; l++)
+        {
+            plane[l+1000][k+1000]=1;
+        }
     }
-    else if (x>=x2 and x<x3)
+    // truck
+    for (int p=y; p<Y; p++)
     {
-
+        for (int q=x; q<X; q++)
+        {
+            plane[q+1000][p+1000]=0;
+        }
     }
-    else if (x>=x3 and x<x4)
+    // counting the places that are 1.
+    int counter=0;
+    for (int a=0; a<2001; a++)
     {
-
+        for (int b=0; b<2001; b++)
+        {
+            if (plane[a][b]==1)
+            {
+                counter+=1;
+            }
+        }
     }
-    else
-    {
-        
-    }
+    cout << counter << endl;
 }
