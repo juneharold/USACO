16feaaaa2d693f3142c939c96b1a077cfafa1bb3
Charrybot <junehahwang@gmail.com>
2021-01-14 21:54:12 +0900

    usaco prac

--- usaco_practice/bronze_A/A_practice/diamond_collector.cpp
+++ usaco_practice/bronze_A/A_practice/diamond_collector.cpp
@@ -6,5 +6,27 @@ using namespace std;
 
 int main()
 {
-
+    int N, K;
+    cin >> N >> K;
+    int diamonds[1000]={};
+    for (int i=0; i<N; i++)
+    {
+        cin >> diamonds[i];
+    }
+    sort(diamonds, diamonds+N);
+    int counter = 0;
+    for (int j=0; j<N; j++)
+    {
+        for (int p=0; p<N; p++)
+        {
+            if (j>=p and diamonds[j]-diamonds[p]<=K)
+            {
+                if (j-p>counter)
+                {
+                counter = j-p+1;
+                }
+            }
+        }
+    }
+    cout << counter << endl;
 }
--- usaco_practice/bronze_B/B_practice/cow_signal.cpp
+++ usaco_practice/bronze_B/B_practice/cow_signal.cpp
@@ -6,5 +6,30 @@ using namespace std;
 
 int main()
 {
-
+    int M, N, K;
+    string signal[10] = {};
+    for (int i=0; i<M; i++)
+    {
+        cin >> signal[i];
+    }
+    // double loop over signal[10]
+    for (int a=0; a<M; a++)
+    {
+        for (int p=0; p<K; p++)
+        {
+            for (int b=0; b<N; b++)
+            {
+                // expand by K
+                for (int j=0; j<K; j++)
+                {
+                    cout << signal[a][b];
+                    if (j==K-1)
+                    {
+                        cout << endl;
+                    }
+                }
+            }
+        }
+    }
 }
+
