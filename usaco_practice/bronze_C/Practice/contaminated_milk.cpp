377eb68faa5cddc9daec80dd2a8a6f08facd9229
Charrybot <junehahwang@gmail.com>
2021-01-18 12:42:49 +0900

    usaco practice

---
+++ usaco_practice/bronze_C/Practice/contaminated_milk.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int N, M, D, S;
+    cin >> N >> M >> D >> S;
+    pair <int, pair<int, int>> PMT[1000]={}; //time, person, milk
+    for (int i=0; i<D; i++)
+    {
+        
+    }
+
+}
