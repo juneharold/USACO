13615d897c8465f5c940e1a3fc498a0c6a6c6b33
Charrybot <junehahwang@gmail.com>
2021-01-24 00:45:33 +0900

    usaco practice

--- usaco_practice/bronze_C/Practice/milk_order.cpp
+++ usaco_practice/bronze_C/Practice/milk_order.cpp
@@ -5,25 +5,24 @@ using namespace std;
 
 int main()
 {
-    freopen("milkorder.in", "r", stdin);
-    freopen("milkorder.out", "w", stdout);
+    //freopen("milkorder.in", "r", stdin);
+    //freopen("milkorder.out", "w", stdout);
     int N, M, K;
     cin >> N >> M >> K;
-    int order[100]={};
-    int fix_pos_in_order[100]={};
+    pair <int, int> order[100]={};
     for (int i=0; i<M; i++)
     {
-        cin >> order[i];
+        cin >> order[i].first; //first is cow num, second is position
     }
-    pair <int, int> fixpos[100]={}; //first-cow, second-position
+    pair <int, int> fixpos[100]={}; //first-cow num, second-position
     for (int i=0; i<K; i++)
     {
         cin >> fixpos[i].first >> fixpos[i].second;
         for (int j=0; j<M; j++)
         {
-            if (fixpos[i].first==order[j])
+            if (fixpos[i].first==order[j].first)
             {
-                fix_pos_in_order[j]=1;
+                order[j].second=fixpos[i].second;
             }
         }
     }
@@ -37,48 +36,65 @@ int main()
     // now find the first zero in array "line", then fill it with 
     for (int i=0; i<M; i++)
     {
-        int cow_num=order[i];
-        int bef=order[i+1];
+        int cow_num=order[i].first;
+        if (order[i].second!=0)
+        {
+            continue;
+        }
+        int aft=-1;
+        int k_min=1;
+        for (int j=i+1; j<M; j++)
+        {
+            if (order[j].second!=0)
+            {
+                aft=order[j].first;
+                break;
+            }
+            k_min += 1;
+        }
+        //cout << k_min << "\n";
+        if (aft==-1) //no hierarchy caows have fixed positions so we can just look for first 0.
+        {
+            break;
+        }
         for (int j=0; j<105; j++)
         {
-            // this is not to have the same number twice
-            int indicate=0;
-            for (int k=0; k<105; k++)
+            if (line[j]==aft)
             {
-                if (line[k]==cow_num)
+                for (int k=k_min; k<100; k++)
                 {
-                    indicate=1;
-                    break;
-                }
-            }
-            if (indicate==0)
-            {
-                if (line[j]==bef)
-                {
-                    for (int k=1; k<100; k++)
+                    if (line[j-k]==0)
                     {
-                        if (line[j-k]==0)
-                        {
-                            line[j-k]=cow_num;
-                            break;
-                        }
+                        line[j-k]=cow_num;
+                        break;
                     }
-                    break;
                 }
+                break;
             }
         }
     }
-    // now find the first 0...
-    int ans;
+    // now find the first 1...
+    int ans=-1, fir_zed=105;
     for (int i=0; i<105; i++)
     {
-        if (line[i]==0)
+        if (line[i]==1)
         {
             ans=i;
             break;
         }
+        if (line[i]==0 and i<=fir_zed)
+        {
+            fir_zed=i;
+        }
     }
-    cout << ans+1;
+    if (ans==-1)
+    {
+        cout << fir_zed+1;
+    }
+    else
+    {
+        cout << ans+1;
+    }
     /* print out line
     for (int i=0; i<10; i++)
     {
@@ -91,11 +107,15 @@ int main()
 5 3
 3 1
 
-8 4 4
+8 4 3
 4 5 6 7
-5 4
+6 4
 3 2
-4 1
 7 6
+
+3 2 1
+2 1
+2 2
+
 
 */
--- usaco_practice/bronze_review/meet.cpp
+++ usaco_practice/bronze_review/meet.cpp
@@ -2,26 +2,45 @@
 #include <algorithm>
 using namespace std;
 
+pair <int, char> Bessie[1000000] = {};
+pair <int, char> Elsie[1000000] = {};
+
 int main()
 {
     int B, E;
     cin >> B >> E;
-    pair <int, char> Bessie[50000] = {};
-    pair <int, char> Elsie[50000] = {};
+    int indexB=0;
     for (int i=0; i<B; i++)
     {
-        cin >> Bessie[i].first >> Bessie[i].second;
+        int time;
+        char direction;
+        cin >> time >> direction;
+        for (int j=0; j<time; j++)
+        {
+            Bessie[indexB].first=1;
+            Bessie[indexB].second=direction;
+            indexB += 1;
+        }
     }
+    int indexE=0;
     for (int i=0; i<E; i++)
     {
-        cin >> Elsie[i].first >> Elsie[i].second;
+        int time;
+        char direction;
+        cin >> time >> direction;
+        for (int j=0; j<time; j++)
+        {
+            Elsie[indexE].first=1;
+            Elsie[indexE].second=direction;
+            indexE += 1;
+        }
     }
     int B_pos=0, E_pos=0;
     int E_fir=2; //
     int moo=0;
-    for (int i=0; i<max(B, E); i++)
+    for (int i=0; i<max(indexB, indexE); i++)
     {
-        if (i<B)
+        if (i<indexB)
         {
             if (Bessie[i].second=='R')
             {
@@ -32,7 +51,7 @@ int main()
                 B_pos -= Bessie[i].first;
             }
         }
-        if (i<E)
+        if (i<indexE)
         {
             if (Elsie[i].second=='R')
             {
@@ -43,7 +62,6 @@ int main()
                 E_pos -= Elsie[i].first;
             }
         }
-        cout << B_pos << E_pos << "\n";
         //
         int og=E_fir;
         if (E_pos>B_pos)
@@ -60,7 +78,7 @@ int main()
         }
         // if og!=E_fir --> add 1
         // if og=2, E_fir=0or1
-        if (og!=E_fir and og!=2 and i>0)
+        if (og!=E_fir and og!=2 and i!=0)
         {
             moo += 1;
         }
@@ -68,6 +86,7 @@ int main()
     cout << moo;
     return 0;
 }
+
 /*
 5 5
 3 L
