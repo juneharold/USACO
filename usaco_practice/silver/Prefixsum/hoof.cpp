d788e377ccd293d11f4cb5c33844db100c95455f
Charrybot <junehahwang@gmail.com>
2021-01-29 15:18:12 +0900

    usaco practice

--- usaco_practice/silver/Prefixsum/hoof.cpp
+++ usaco_practice/silver/Prefixsum/hoof.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 char FJ[100005]={};
+char A[3][100005]={};
 
 int main()
 {
@@ -15,63 +16,11 @@ int main()
     cin >> N;
     for (int i=1; i<=N; i++)
     {
-        cin >> FJ[i];
+        char a;
+        cin >> a;
+        if (a=='H') A[0][i]++;
+        if (a=='P') A[1][i]++;
     }
-    char Bess1[3]={'H', 'P', 'S'};
-    char Bess2[3]={'H', 'P', 'S'};
-    int max=0;
-    for (int i=0; i<3; i++)
-    {
-        for (int j=0; j<3; j++)
-        {
-            char first, second;
-            first=Bess1[i];
-            second=Bess2[j];
-            for (int k=1; k<=N; k++)
-            {
-                int cut=k;
-                int score=0;
-                for (int q=1; q<=N; q++)
-                {
-                    if (q<=k) // first
-                    {
-                        if (Bess1[q]=='H' and FJ[q]=='S')
-                        {
-                            score += 1;
-                        }
-                        else if (Bess1[q]=='S' and FJ[q]=='P')
-                        {
-                            score += 1;
-                        }
-                        else if (Bess1[q]=='P' and FJ[q]=='H')
-                        {
-                            score += 1;
-                        }
-                    }
-                    else
-                    {
-                        if (Bess1[q]=='H' and FJ[q]=='S')
-                        {
-                            score += 1;
-                        }
-                        else if (Bess1[q]=='S' and FJ[q]=='P')
-                        {
-                            score += 1;
-                        }
-                        else if (Bess1[q]=='P' and FJ[q]=='H')
-                        {
-                            score += 1;
-                        }
-                    }
-                }
-                if (score > max)
-                {
-                    max=score;
-                }
-            }
-        }
-    }
-    cout << max;
 }
 
 
