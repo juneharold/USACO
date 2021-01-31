17c82bb70df87e5e8ec9454ab1727712526982ac
Charrybot <junehahwang@gmail.com>
2021-01-31 22:14:57 +0900

    usaco practice

---
+++ usaco_practice/silver/Prefixsum/MaxSubArr.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int T;
+    cin >> T;
+    for (int i=0; i<T; i++)
+    {
+        int N;
+        cin >> N;
+        
+    }
+}
--- usaco_practice/silver/Prefixsum/lazybear.cpp
+++ usaco_practice/silver/Prefixsum/lazybear.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+int arr[1000005]={};
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
+    int N, K;
+    cin >> N >> K;
+    for (int i=1; i<=N; i++)
+    {
+        int g, x;
+        cin >> g >> x;
+        if (x-K<1) arr[1]+=g;
+        else arr[x-K]+=g;
+        if (x+K>1000004) arr[1000004]-=g;
+        else arr[x+K+1]-=g;
+    }
+    int sum=0;
+    int max=0;
+    for (int i=1; i<=1000005; i++)
+    {
+        sum+=arr[i];
+        if (sum>max)
+        {
+            max=sum;
+        }
+    }
+    cout << max;
 }
+
+
