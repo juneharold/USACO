d78cad0ee396978691683c29addc6f2fa792cc26
Juneha Hwang <junehahwang@gmail.com>
2021-02-20 16:55:36 +0900

    set map

--- usaco_practice/silver/set_map/배부른_마라토너.cpp
+++ usaco_practice/silver/set_map/배부른_마라토너.cpp
@@ -7,17 +7,23 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    set <string> names1;
+    set <string> unique;
+    multiset <string> names1;
     int N; cin >> N;
     for (int i=0; i<N; i++)
     {
         string a; cin >> a;
+        unique.insert(a);
         names1.insert(a);
     }
     for (int i=0; i<N-1; i++)
     {
         string a; cin >> a;
-        names1.erase(a);
+        names1.insert(a);
+        if (names1.count(a)%2==0) 
+        {
+            names1.erase(a);
+        }
     }
     auto iter=names1.begin();
     cout << *iter;
