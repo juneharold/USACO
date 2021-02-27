6718e8eeabfdda7fab5bafd8201e848239455ad3
Juneha Hwang <junehahwang@gmail.com>
2021-02-27 16:53:09 +0900

    prac

--- Live_Contests/Dec_2020/cowntagion.cpp
+++ Live_Contests/Dec_2020/cowntagion.cpp
@@ -5,6 +5,27 @@ using namespace std;
 
 vector <int> graph[100005];
 int numcows[100005]={};
+int days=0;
+
+void dfs(int cur)
+{
+    numcows[cur]=1;
+    while (numcows[cur]<graph[cur].size())
+    {
+        numcows[cur]*=2;
+        days++;
+    }
+    for (int j=0; j<graph[cur].size(); j++)
+    {
+        int next=graph[cur][j];
+        if (numcows[next]==0) 
+        {
+            days++;
+            numcows[cur]-=1;
+            dfs(next);
+        }
+    }
+}
 
 int main()
 {
@@ -15,5 +36,15 @@ int main()
         graph[a].push_back(b);
         graph[b].push_back(a);
     }
-
+    dfs(1);
+    cout << days;
 }
+
+/*
+3
+1 2
+1 3
+
+*/
+
+
