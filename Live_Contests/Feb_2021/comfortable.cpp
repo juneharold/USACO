675ea6d2a53b1c7bb1b6bbcee51564be2ac3a4b0
Juneha Hwang <junehahwang@gmail.com>
2021-03-03 06:43:55 +0900

    hjk

---
+++ Live_Contests/Feb_2021/comfortable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int cowloc[1005][1005]={};
+int adjcow[1005][1005]={};
+int dx[4]={1, -1, 0, 0};
+int dy[4]={0, 0, 1, -1};
+
+int main()
+{
+    int N; cin >> N;
+    for (int i=0; i<N; i++)
+    {
+        int x, y; cin >> x >> y;
+
+    }
+}
---
+++ Live_Contests/Feb_2021/green.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int arr[505][505]={};
+
+int main()
+{
+    int N; cin >> N;
+    for (int i=0; i<N; i++)
+    {
+        for (int j=0; j<N; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+    
+}
