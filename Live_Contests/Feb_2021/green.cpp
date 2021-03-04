4c6a5b8e99cbec855b1bcbb5976134490bfcc192
Juneha Hwang <junehahwang@gmail.com>
2021-03-05 01:27:21 +0900

    practice

--- Live_Contests/Feb_2021/green.cpp
+++ Live_Contests/Feb_2021/green.cpp
@@ -3,16 +3,51 @@
 using namespace std;
 
 int arr[505][505]={};
+int pfs100[505][505]={};
+int pfs0[505][505]={};
 
 int main()
 {
     int N; cin >> N;
-    for (int i=0; i<N; i++)
+    int count100=0;
+    for (int i=1; i<N; i++)
     {
-        for (int j=0; j<N; j++)
+        for (int j=1; j<=N; j++)
         {
             cin >> arr[i][j];
+            if (arr[i][j]==100) count100++;
+            pfs100[i][j]=count100+pfs100[i-1][j];
+        }
+        count100=0;
+    }
+    int counter=0;
+    for (int Y=1; Y<N; Y++)
+    {
+        for (int y=0; y<N-1; y++)
+        {
+            for (int X=1; X<N; X++)
+            {
+                for (int x=0; x<N-1; x++)
+                {
+                    int min=200;
+                    for (int yy=y; yy<=Y; yy++)
+                    {
+                        for (int xx=x; xx<=X; xx++)
+                        {
+                            if (arr[yy][xx]<min) min=arr[yy][xx];
+                        }
+                    }
+                    if (min==100) counter++;
+                }
+            }
         }
     }
-    
+    cout << counter;
 }
+
+/*
+3
+57 120 87
+200 100 150
+2 141 135
+*/
---
+++ Live_Contests/Feb_2021/year.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int ancestor[100005]={};
+
+int main()
+{
+    int N, K; cin >> N >> K;
+    for (int i=0; i<N; i++) cin >> ancestor[i];
+    sort(ancestor, ancestor+N);
+    // 구간 개수: K-1
+    int point=0;
+    while (point<ancestor[N-1])
+    {
+        point+=12;
+    }
+    int end=point;
+    int range[100000]={};  // (0, 12) (12, 24) .....
+    for (int i=0; i<N; i++)
+    {
+        range[ancestor[i]/12]+=1;
+    }
+    int subtract[100000]={};
+    int index=0;
+    int subt=0;
+    for (int i=0; i<(end/12+1); i++)
+    {
+        if (range[i]==0)
+        {
+            subt+=12;
+        }
+        else
+        {
+            subtract[index]=subt;
+            index++;
+            subt=0;
+        }
+    }
+    sort(subtract, subtract+index);
+    int min;
+    if (index<=K) min=0;
+    else min=index-K-1;
+    for (int i=index-1; i>=min; i--)
+    {
+        end-=subtract[i];
+    }
+    cout << end;
+}
+
+
+/*
+5 3
+101
+85
+100
+46
+95
+
+5 3
+101
+85
+46
+6
+11 
+*/
+
+
+
