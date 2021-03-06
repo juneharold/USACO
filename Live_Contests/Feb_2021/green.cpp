b89895875bcdf7eaf62968ea7fae656d747e0354
Juneha Hwang <junehahwang@gmail.com>
2021-03-06 11:44:23 +0900

    puhh

--- Live_Contests/Feb_2021/green.cpp
+++ Live_Contests/Feb_2021/green.cpp
@@ -9,16 +9,19 @@ int pfs0[505][505]={};
 int main()
 {
     int N; cin >> N;
-    int count100=0;
+    int count100=0, count0=0;
     for (int i=1; i<N; i++)
     {
         for (int j=1; j<=N; j++)
         {
             cin >> arr[i][j];
             if (arr[i][j]==100) count100++;
+            if (arr[i][j]<100) count0++;
             pfs100[i][j]=count100+pfs100[i-1][j];
+            pfs0[i][j]=count0+pfs0[i-1][j];
         }
         count100=0;
+        count0=0;
     }
     int counter=0;
     for (int Y=1; Y<N; Y++)
@@ -29,15 +32,11 @@ int main()
             {
                 for (int x=0; x<N-1; x++)
                 {
-                    int min=200;
-                    for (int yy=y; yy<=Y; yy++)
-                    {
-                        for (int xx=x; xx<=X; xx++)
-                        {
-                            if (arr[yy][xx]<min) min=arr[yy][xx];
-                        }
-                    }
-                    if (min==100) counter++;
+                    // 100 exists
+                    // less than 100 does not exist
+                    int num100=pfs100[Y][X]-pfs100[y-1][X]-pfs100[Y][x-1]+pfs100[y-1][x-1];
+                    int num0=pfs0[Y][X]-pfs0[y-1][X]-pfs0[Y][x-1]+pfs0[y-1][x-1];
+                    if ((num100>0) and (num0=0)) counter++;
                 }
             }
         }
