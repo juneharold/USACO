e141fc8ee7588b78cbf9dcb1494fb1dedd689f5d
Juneha Hwang <junehahwang@gmail.com>
2021-03-26 00:02:00 +0900

    kickstart

--- Kickstart/2021_A/rabbit.cpp
+++ Kickstart/2021_A/rabbit.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <set>
 using namespace std;
+
+int dy[4]={0, 0, 1, -1};
+int dx[4]={1, -1, 0, 0};
 
 int main()
 {
@@ -9,24 +14,65 @@ int main()
   {
     int R, C; cin >> R >> C;
     int grid[305][305]={};
-    for (int i=1; i<=R; i++)
-    {
-      for (int j=1; j<=C; j++)
-      {
-        cin >> grid[i][j];
-      }
-    }
+    for (int i=1; i<=R; i++) for (int j=1; j<=C; j++) cin >> grid[i][j];
+    int prev=-1;
     int counter=0;
-    for (int i=1; i<=R; i++)
+    multiset pair< int, pair <int, int> > s;
+    int visit[305][305]={};
+    while (counter>prev)
     {
-      for (int j=1; j<=C; j++)
+      prev=counter;
+      // 가장 큰 칸 찾기
+      int max_space=0;
+      int I, J;
+      for (int i=1; i<=R; i++) for (int j=1; j<=C; j++)
       {
-        // right
-        int right_diff=abs(grid[i][j]-grid[i][j+1]);
-        // left
-        // up
-        // down
+        if (grid[i][j]>max_space and visit[i][j]==0)
+        {
+          max_space=grid[i][j];
+          I=i; J=j;
+        }
+      }
+      visit[I][J]
+      // 가장 큰 칸에서 올리기
+      for (int i=1; i<=R; i++) for (int j=1; j<=C; j++)
+      {
+        // 가장 큰 칸일때
+        if (grid[i][j]==max_space)
+        {
+          for (int d=0; d<4; d++)
+          {
+            if ((i+dy[d]>=1 and i+dy[d]<=R) and (j+dx[d]>=1 and j+dx[d]<=C))
+            {
+              int diff=grid[i][j]-grid[i+dy[d]][j+dx[d]];
+              if (diff>1)
+              {
+                grid[i+dy[d]][j+dx[d]]+=(diff-1);
+                counter+=(diff-1);
+              }
+            }
+          }
+        }
       }
     }
+    cout << "Case #" << t << ": " << counter << "\n";
   }
 }
+
+/*
+3
+1 3
+3 4 3
+1 3
+3 0 0
+3 3
+0 0 0
+0 2 0
+0 0 0
+
+1
+3 3
+5 1 1
+1 2 1
+4 1 1
+*/
---
+++ usaco_practice/silver/silv_practice/mooyoo.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dx[4]={1, -1, 0, 0};
+int dy[4]={0, 0, 1, -1};
+int graph[105][15]={};
+int visit[105][15]={};
+
+void dfs(int Y, int X, int color)
+{
+  visit[Y][X]=1;
+  for (int i=0; i<4; i++)
+  {
+    int next_Y=Y+dy[i], next_X=X+dx[i];
+    if (visit[next_Y][next_X]==0 and graph[next_Y][next_X]==color) dfs(next_Y, next_X, color);
+  }
+}
+
+int main()
+{
+  int N, K; cin >> N >> K;
+  for (int i=1; i<=N; i++) for (int j=1; j<=10; j++)  cin >> graph[i][j];
+  // dfs
+  for (int y=1; y<=N; y++) for (int x=1; x<=10; x++)
+  {
+    int color=graph[y][x];
+    if (color!=0)
+    {
+      dfs(y, x, color);
+    }
+
+  }
+  /*
+  for (int i=1; i<=N; i++)
+  {
+    for (int j=1; j<=10; j++)
+    {
+      cout << graph[i][j];
+    }
+    cout << "\n";
+  }
+  */
+}
