a7db8e9fb09fc2c77569051b6415ed49790df2a2
Juneha Hwang <junehahwang@gmail.com>
2021-03-27 13:12:07 +0900

    practice

--- usaco_practice/silver/dfs2/트리와쿼리.cpp
+++ usaco_practice/silver/dfs2/트리와쿼리.cpp
@@ -3,11 +3,11 @@
 #include <vector>
 using namespace std;
 
-int parent[1000000]={};
 int visit[1000000]={};
+int size[1000000]={};
 vector <int> tree[1000000];
 
-void find_parent(int cur)
+void cnt_child(int cur)
 {
     visit[cur]=1;
     for (int j=0; j<tree[cur].size(); j++)
@@ -15,24 +15,35 @@ void find_parent(int cur)
         int next=tree[cur][j];
         if (visit[next]==0)
         {
-            parent[next]=cur;
-            find_parent(next);
+            cnt_child(next);
         }
     }
+    for (int j=0; j<tree[cur].size(); j++)
+    {
+        int next=tree[cur][j];
+        size[cur]+=size[next];
+    }
+    size[cur]++;
 }
+
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n; cin >> n;
-    for (int i=0; i<n-1; i++)
+    int N, R, Q; cin >> N >> R >> Q;
+    for (int i=0; i<N-1; i++)
     {
         int a, b; cin >> a >> b;
         tree[a].push_back(b);
         tree[b].push_back(a);
     }
-    find_parent(1);
-    for (int i=2; i<=n; i++) cout << parent[i] << "\n";
+    cnt_child(R);
+    for (int i=0; i<Q; i++)
+    {
+        int q; cin >> q;
+        // q의 부모 찾기
+        cout << size[q] << "\n";
+    }
     return 0;
 }
