df94ae2b125afa2c047cd50c7033febf5062ee9f
Juneha Hwang <junehahwang@gmail.com>
2021-04-01 20:27:09 +0900

    sheeps

--- usaco_practice/silver/dfs2/양구출작전.cpp
+++ usaco_practice/silver/dfs2/양구출작전.cpp
@@ -5,38 +5,36 @@ using namespace std;
 
 pair <char, int> SheWol[123460]={}; //first는 wolf or sheep, second는 마릿수 저장
 int visit[123460]={};
+long long size[123460]={};
 vector <int> tree[123460];
-long long sheep_num=0;
-long long fucking_wolves[123460]={};
-long long wolves=0;
 
-void cnt_sheep(int cur, int wolf)
+void cnt_sheep(int cur)
 {
     visit[cur]=1;
-    if (SheWol[cur].first=='S' and SheWol[cur].second>=wolf)
-    {
-        sheep_num+=SheWol[cur].second-wolf;
-        wolves=0;
-    }
-    else if (SheWol[cur].first=='S' and SheWol[cur].second<wolf)
-    {
-        wolves-=SheWol[cur].second;
-    }
-    else
-    {
-        wolves+=SheWol[cur].second;
-    }
-    fucking_wolves[cur]=wolves;
     for (int j=0; j<tree[cur].size(); j++)
     {
         int next=tree[cur][j];
-        if (visit[next]==0) cnt_sheep(next, fucking_wolves[cur]);
-        /*
-        만약 갈랫 섬에 돌아간다면 wolf 수는 리셋된다.
-        아이디어:
-        각 섬마다 wolf 누적합을 array에 저장함.
-        */
-        fucking_wolves[cur]=wolves;
+        if (visit[next]==0)
+        {
+            cnt_sheep(next);
+        }
+    }
+    for (int j=0; j<tree[cur].size(); j++)
+    {
+        int next=tree[cur][j];
+        size[cur]+=size[next];
+    }
+    if (SheWol[cur].first=='W' and SheWol[cur].second>size[cur])
+    {
+        size[cur]=0;
+    }
+    else if (SheWol[cur].first=='W' and SheWol[cur].second<size[cur])
+    {
+        size[cur]-=SheWol[cur].second;
+    }
+    else if (SheWol[cur].first=='S')
+    {
+        size[cur]+=SheWol[cur].second;
     }
 }
 
@@ -52,9 +50,8 @@ int main()
         SheWol[i].first=t;
         SheWol[i].second=a;
     }
-    cnt_sheep(1, 0);
-    for (int i=1; i<=N; i++) cout << fucking_wolves[i] << "\n";
-    cout << sheep_num;
+    cnt_sheep(1);
+    cout << size[1];
     return 0;
 }
 
