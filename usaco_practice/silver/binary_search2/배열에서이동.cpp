4686d3ba01c05b6f33dbd457c6490ffadf8358af
Juneha Hwang <junehahwang@gmail.com>
2021-04-08 14:37:27 +0900

    bs2

---
+++ usaco_practice/silver/binary_search2/배열에서이동.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+}
+
+/*
+1) 최대 혹은 최소인지 확인,
+2) 최대 최소 차가 mid이하면 이동 (dfs사용)
+3) 만약 끝까지 갈 수 있으면 high=mid
+4) 끝까지 갈 수 없으면 low=mid
+*/
