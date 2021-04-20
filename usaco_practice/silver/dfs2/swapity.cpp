dbb08a6db7804d650faf4780641eb3e3ab8ce60b
Charrybot <junehahwang@gmail.com>
2021-04-20 12:11:15 +0900

    dfs2

--- usaco_practice/silver/dfs2/swapity.cpp
+++ usaco_practice/silver/dfs2/swapity.cpp
@@ -5,9 +5,26 @@ m-step process를 한 번 실행 했을때 각 칸이 어디로 가는지 알아
 그렇게 되면 k%최소공배수 실행.
 */
 #include <iostream>
+#include <algorithm>
 using namespace std;
+
+int n, m, k;
+pair <int, int> seq[105]={};
+int arr[100005]={};
 
 int main()
 {
-    cout << "hello word";
+    cin >> n >> m >> k;
+    for (int i=1; i<=n; i++) arr[i]=i;
+    for (int i=0; i<m; m++) {
+        int a, b; cin >> a >> b;
+        seq[i].first=a;
+        seq[i].second=b;
+    }
+    cout << seq[0].first;
+    for (int i=0; i<m; m++) {
+        int from=seq[i].first, to=seq[i].second;
+        reverse(&arr[from], &arr[to+1]);
+    }
+    for (int i=1; i<=n; i++) cout << arr[i];
 }
