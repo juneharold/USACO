862f173792f7a1775219a4319a20ce033b9a0a8d
Charrybot <junehahwang@gmail.com>
2022-06-03 08:52:34 +0900

    acsl

--- ACSL/필기.cpp
+++ ACSL/필기.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
+#include <map>
 using namespace std;
 
+int arr[100005]={};
+map<int, int> primes;
+
 int f(int x) {
-    if(x>=10) return f(f(x-3))+3;
-    else if (4<x && x<10) return f(x-2)-2;
-    else return x-1;
+    if(x%2==1 && primes[x]==1) return f(x-2)+1;
+    else if (x%2==0) return f(x/2)+3;
+    else return x+1;
 }
 
 string decToOctal(int n){
@@ -24,17 +28,58 @@ string decToOctal(int n){
     return ret;
 }
 
+string decToHexa(int n)
+{
+    char hexaDeciNum[100];
+    int i = 0;
+    while (n != 0) {
+        int temp = 0;
+        temp = n % 16;
+        if (temp < 10) {
+            hexaDeciNum[i] = temp + 48;
+            i++;
+        }
+        else {
+            hexaDeciNum[i] = temp + 55;
+            i++;
+        }
+        n = n / 16;
+    }
+    string ret;
+    for (int j = i - 1; j >= 0; j--)
+        ret+=hexaDeciNum[j];
+    return ret;
+}
+
+int p[100]={};
+
 int main()
 {
-    int cnt=0;
-    for (int i=2001; i<=2021; i++) {
-        string oct=decToOctal(i);
-        for (int i=0; i<oct.size(); i++) {
-            if (oct[i]>'4') cnt++;
+    /*
+    for (int i=2; i<=10000; i++) arr[i]=i;
+
+    for (int i=2; i<=10000; i++) {
+        if (arr[i]!=0) {
+            int from=i*i;
+            for (int j=from; j<=10000; j+=i) arr[j]=0;
+        }
+    }
+    for (int i=2; i<=10000; i++) if(arr[i]!=0) primes[i]=1;
+
+    cout << f(f(f(f(20))));*/
+
+    int cnt=0, fib[20]={};
+    fib[0]=0; fib[1]=1;
+    for (int i=2; i<=19; i++) fib[i]=fib[i-1]+fib[i-2];
+    for (int x=3; x<=6; x++) {
+        int f=fib[x];
+        for (int y=x+1; y<=19; y++) {
+            if (fib[y]%f==0) cnt++;
         }
     }
     cout << cnt;
 }
+
 
 /*
 2
