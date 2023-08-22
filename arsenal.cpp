bfc503301439a671264849155f39507e57e34bfa
Charrybot <junehahwang@gmail.com>
2023-08-22 15:02:07 +0900

    update_arsenal

--- arsenal.cpp
+++ arsenal.cpp
@@ -17,7 +17,7 @@ typedef long long ll;
 typedef pair<int, int> pii;
 #define fs first
 #define sc second
-const ll nmax=2e5+5, sqrmax=635, MOD=1e9+7;
+const ll nmax=2e5+5, sqrmax=635, MOD=1e9+7, inf=1e18;
 
 ll fastpow (ll x, ll y) {
     ll ret=1;
@@ -95,7 +95,7 @@ void solve() {
 int main()
 {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int T; cin >> T;
+    int T=1; cin >> T;
     while (T--) {
         solve();
     }
